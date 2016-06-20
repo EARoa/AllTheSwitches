7d04228ee030971dcf6f52c99ffc3698a7921c26
Efrain <jesusayllonroa@gmail.com>
2016-06-20 14:09:17 -0500

    Workinggit add .

--- AllTheSwitches/ViewController.m
+++ AllTheSwitches/ViewController.m
@@ -15,8 +15,6 @@
 @property (nonatomic, weak) IBOutlet UISwitch *characterRange;
 @property (nonatomic, weak) IBOutlet UISwitch *shouldClear;
 @property (nonatomic, weak) IBOutlet UISwitch *shouldReturn;
-
-
 @property (nonatomic, weak) IBOutlet UITextField *textField;
 
 @end
@@ -32,5 +30,89 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    
+    
+    UISwitch *mySwitch = (self.beginEditing);
+    if ([mySwitch isOn]) {
+        NSLog(@"Begin Editing!");
+        return TRUE;
+    }
+    
+    
+    [textField resignFirstResponder];
+    return YES;
+    
+    
+}
+
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    
+    
+    UISwitch *mySwitch = (self.endEditing);
+    if ([mySwitch isOn]) {
+        NSLog(@"End Editing!");
+        return TRUE;
+    }
+    
+    
+    [textField resignFirstResponder];
+    return YES;
+    
+}
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string{
+    
+    UISwitch *mySwitch = (self.characterRange);
+    if ([mySwitch isOn]) {
+        NSLog(@"Change Characters?");
+        return TRUE;
+    }
+    
+    
+    [textField resignFirstResponder];
+    return YES;
+    
+    
+    
+}
+
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField{
+    
+    
+    UISwitch *clearSwitch = (self.shouldClear);
+    if ([clearSwitch isOn]) {
+        NSLog(@"CLEAR!");
+    }
+    
+    [textField resignFirstResponder];
+    return YES;
+    
+}
+
+
+
+-(BOOL) textFieldShouldReturn:(UISwitch *)textField{
+    
+    UISwitch *mySwitch = (self.shouldReturn);
+    if ([mySwitch isOn]) {
+        NSLog(@"RETURN!");
+        return TRUE;
+    }
+    
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
 
 @end
